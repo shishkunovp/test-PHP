8e129b6d78179c826d90b615b7e79203992fe30d
shishkunovp <147750399+shishkunovp@users.noreply.github.com>
2023-10-20 23:09:29 +0500

    Задание 5

--- index.php
+++ index.php
@@ -2,51 +2,84 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Задание 3</title>
+    <title>Задание 5</title>
 </head>
 <body>
 <?php
+function setCacheCurrencyExchange($currency)
+{
+    if (file_exists("cache.txt"))
+    {
+        $cache = fopen("cache.txt", "w");
+        fwrite($cache, "$currency\n");
+        fclose($cache);
+        echo "В файл cache.txt добавлена запись\n";
+    }
+    else echo "Файл cache.txt не найден\n";
+}
+function viewCacheCurrencyExchange()
+{
+    if (file_exists("cache.txt"))
+    {
+        $cache = fopen("cache.txt", "r");
+        $result = fgets($cache);
+        echo $result;
+    }
+    else echo "Файл cache.txt не найден\n";
+}
+
 class Singleton
 {
-    private static $instances = [];
+    private static $instances;
     protected function __construct() { }
     public static function getInstance()
     {
         $cls = static::class;
-        if (!isset(self::$instances[$cls])) {
-            self::$instances[$cls] = new static();
+        if (!isset(self::$instances)) {
+            self::$instances = new static();
         }
-        return self::$instances[$cls];
+        return self::$instances;
     }
 }
-class VisitLog extends Singleton
+class CurrencyExchange extends Singleton
 {
-    private static $visits = [];
-
-    public function logVisit($visitor,$ip)
+    private static $rates;
+    public $data;
+    public function getExchangeRate()
     {
-         self::$visits[$visitor] = $ip;
+        if (self::$rates === null) {
+            self::$rates = json_decode(file_get_contents('https://www.cbr-xml-daily.ru/daily_json.js'));
+        }
+        return self::$rates;
     }
-
-    public function showVisit()
+    public function updateExchangeRate($value)
     {
-        echo "Журнал посещений:<br>";
-        foreach (self::$visits as $key=>$value)
+        $this->data = $this->getExchangeRate();
+        setCacheCurrencyExchange("Обменный курс $value по ЦБ РФ на сегодня: {$this->data->Valute->$value->Value}\n");
+    }
+    public function showExchangeRate($value)
+    {
+        echo "Обменный курс $value по ЦБ РФ на сегодня: {$this->data->Valute->$value->Value}\n";
+    }
+}
+function clientCode($valute)
+{
+    $date = getdate();
+    $usd1 = CurrencyExchange::getInstance();
+        if ($date["seconds"] > 30)
         {
-            echo "Пользователь: $key IP адрес: $value<br>";
+            try {
+                if (false == ($error = $usd1->getExchangeRate())) throw new Exception("сервер не доступен");
+                    $usd1->updateExchangeRate($valute);
+                    $usd1->showExchangeRate($valute);
+                } catch (Exception $e){
+                    echo $e->getMessage();
+                    echo $e->getLine();
+                }
         }
-    }
+        else {viewCacheCurrencyExchange();}
 }
-function clientCode()
-{
-    $s1 = VisitLog::getInstance();
-    $s1->logVisit("andrey", "10.162.154.211");
-    $s1->logVisit("ivan", "123.162.231.124");
-    $s1->logVisit("sergey", "198.162.124.166");
-    $s1->logVisit("pavel", "145.125.255.254");
-    $s1->showVisit();
-    }
-clientCode();
+clientCode("EUR");
 ?>
 </body>
 </html>
